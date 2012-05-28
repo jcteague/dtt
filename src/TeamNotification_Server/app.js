27e08292e2f6da2b5b628bd5075b7c1ee1095754
Raymi <jcruz@intellisys.com.do>
2012-05-28 14:36:38 -0400

    creating basic options request for root page

--- src/TeamNotification_Server/app.js
+++ src/TeamNotification_Server/app.js
@@ -1,29 +1,16 @@
 var express = require('express');
-var redis = require('redis');
-var client = redis.createClient();
-
-client.on('error',function(err){
-   console.log('error');
-   console.log(err);
-});
-
 var app= express.createServer();
+app.use(express.bodyParser());
+app.use(express.methodOverride());
 
-app.get('/',function(req,res){
-    var result = {
-        name:'raymi',
-        id: 1
-    };
-
-    var userId = req.param('userId');
-    var userName = req.param('userName');
-    var message = req.param('userMessage');
-
-    var full_message = userName + '-' + message;
-    console.log('publishing message: ' + full_message);
-    client.publish('chat1',full_message);
-    res.send(full_message);
+app.options('/',function(req,res){
+    r = {
+        get: [ { type: 'get_rooms', url: '/user/rooms'} ],
+        post: [ { type: 'create_room', url: '/room'} ]
+    }
+    res.send(r);
 });
-
 
 app.listen(3000);
+
+
