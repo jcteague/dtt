97e09d2992d2363997e61b9e457f95004fe1d145
Jorge Ferreira <jorge@tn-jorfx-VirtualBox.(none)>
2012-05-29 17:31:03 -0400

    change file structure

---
+++ src/TeamNotification_Server/routes/room.js
@@ -0,0 +1,6 @@
+module.exports = function(app){
+    app.post('/room',function(req, res){
+
+    });
+};
+
--- src/TeamNotification_Server/routes/user/index.js
+++
@@ -1,5 +0,0 @@
-var helper = require('core_helper');
-module.exports = function(app){
-    helper.require_all_files_in(__dirname,app)
-};
-
