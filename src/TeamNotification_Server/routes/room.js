1ed1dc12aefbd9488bcb9122f06e1a563a0773ab
Jorge Ferreira <jorge@tn-jorfx-VirtualBox.(none)>
2012-05-29 17:57:43 -0400

    creating chatroom post function and migration

--- src/TeamNotification_Server/routes/room.js
+++ src/TeamNotification_Server/routes/room.js
@@ -1,6 +1,18 @@
+var pg = require('pg');
+var connectionString = "postgres://postgres:1234@localhost/dtt_main";
+var client_db = new pg.Client(connectionString);
+
+function verify (value){
+	if(!value) next(new Error(500,"blah"));
+}
+
 module.exports = function(app){
     app.post('/room',function(req, res){
 
+	var name = req.param('name');
+	verify(name);
+	client_db.query("INSERT INTO chat_room (name) VALUES($1)", [name]);
+	
     });
 };
 
