f3a86ba011d5e813aeb7481ff050b3e55b8ad042
Jorge Ferreira <jferreira@intellisys.com.do>
2012-05-31 09:49:52 -0400

    testing pg connection creating chat room table

--- src/TeamNotification_Server/routes/room.js
+++ src/TeamNotification_Server/routes/room.js
@@ -1,18 +1,19 @@
+
 var pg = require('pg');
 var connectionString = "postgres://postgres:1234@localhost/dtt_main";
 var client_db = new pg.Client(connectionString);
-
-function verify (value){
-	if(!value) next(new Error(500,"blah"));
-}
+client_db.connect();
 
 module.exports = function(app){
-    app.post('/room',function(req, res){
-
-	var name = req.param('name');
-	verify(name);
-	client_db.query("INSERT INTO chat_room (name) VALUES($1)", [name]);
 	
+    app.post('/room',function(req, res, next){
+	
+	var name = req.param('name');
+	console.log(name);
+	if(!name){ next( new Error(500,"blah"));}
+	client_db.query("INSERT INTO chat_room(name) VALUES($1)", [name]);
+	res.send('room created');
+
     });
 };
 
