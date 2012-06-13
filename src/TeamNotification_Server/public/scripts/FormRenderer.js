ac77bd2b952e36f3964414e9734c7059c6b969af
John Teague <jcteague@gmail.com>
2012-06-13 08:31:51 -0500

    building form renderer

---
+++ src/TeamNotification_Server/public/scripts/FormRenderer.js
@@ -0,0 +1,10 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: jcteague
+ * Date: 6/12/12
+ * Time: 9:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+var FormRenderer = function(collection){
+
+}
