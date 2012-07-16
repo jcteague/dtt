6453f5eb1610ffb1da51597f940df06db964ca53
Eddy Espinal <eespinal@intellisys.com.do>
2012-07-16 17:26:01 -0400

    Adding redis setup scripts

---
+++ run_redis.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+./redis-2.4.15/src/redis-server
---
+++ setup_redis.sh
@@ -0,0 +1,5 @@
+wget http://redis.googlecode.com/files/redis-2.4.15.tar.gz &&
+tar xzf redis-2.4.15.tar.gz &&
+cd redis-2.4.15 &&
+make &&
+rm -f ../redis-2.4.15.tar.gz
