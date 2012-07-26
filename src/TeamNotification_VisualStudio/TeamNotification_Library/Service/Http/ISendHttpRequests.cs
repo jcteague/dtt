fd51d530219748c289cbe2c9a9e219e1c6cd3b9f
Eddy Espinal <eespinal@intellisys.com.do>
2012-07-26 17:48:05 -0400

    Attempting to deserialize string with wrong function

--- src/TeamNotification_VisualStudio/TeamNotification_Library/Service/Http/ISendHttpRequests.cs
+++ src/TeamNotification_VisualStudio/TeamNotification_Library/Service/Http/ISendHttpRequests.cs
@@ -9,8 +9,6 @@ namespace TeamNotification_Library.Service.Http
 
         void Get(string uri, Action<Task<string>> action);
         
-        //Task Get<T>(string uri, Action<T> action) where T : class;
-        
         Task<R> Get<T, R>(string uri, Func<T, R> action) where T : class;
     }
 }
--- src/TeamNotification_VisualStudio/TeamNotification_Package/DynamicControlBuilder.cs
+++ src/TeamNotification_VisualStudio/TeamNotification_Package/DynamicControlBuilder.cs
@@ -23,16 +23,52 @@ namespace AvenidaSoftware.TeamNotification_Package
             this.serializer = serializer;
         }
 
-//        public Panel GetContentFrom(string href)
-//        {
-//            var panel = new StackPanel();
-//            return httpClient.Get(href, callbackFactory.BuildFor(panel)).Result;
-//            return panel;
-//        }
-        
         public Panel GetContentFrom(string href)
         {
-            return httpClient.Get(href, x => serializer.Deserialize<Collection>(x.Result)).Result;
+            var collection = httpClient.Get<string, Collection>(href, Callback).Result;
+            return BuildContent();
+        }
+
+        private Collection Callback(string response)
+        {
+            return serializer.Deserialize<Collection>(response);
+        }
+
+        private Panel BuildContent()
+        {
+            // Create a StackPanel and set its properties
+            StackPanel panel = new StackPanel();
+            panel.Width = 300;
+            panel.Height = 200;
+            panel.Background = new SolidColorBrush(Colors.LightBlue);
+            panel.Orientation = Orientation.Horizontal;
+
+            // Create Ellipses and add to StackPanel
+            Ellipse redCircle = new Ellipse();
+            redCircle.Width = 100;
+            redCircle.Height = 100;
+            redCircle.Fill = new SolidColorBrush(Colors.Red);
+            panel.Children.Add(redCircle);
+
+            Ellipse yellowCircle = new Ellipse();
+            yellowCircle.Width = 60;
+            yellowCircle.Height = 60;
+            yellowCircle.Fill = new SolidColorBrush(Colors.Yellow);
+            panel.Children.Add(yellowCircle);
+
+            Ellipse greenCircle = new Ellipse();
+            greenCircle.Width = 40;
+            greenCircle.Height = 40;
+            greenCircle.Fill = new SolidColorBrush(Colors.Green);
+            panel.Children.Add(greenCircle);
+
+            Ellipse blueCircle = new Ellipse();
+            blueCircle.Width = 20;
+            blueCircle.Height = 20;
+            blueCircle.Fill = new SolidColorBrush(Colors.Blue);
+            panel.Children.Add(blueCircle);
+
+            return panel;
         }
     }
 }
