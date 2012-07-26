44a826b6cea4c29fe07db3ecff50960c6604618e
Eddy Espinal <eespinal@intellisys.com.do>
2012-07-26 10:56:54 -0400

    Setting up first tests

---
+++ src/TeamNotification_VisualStudio/TeamNotification_Library/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamNotification_Library.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string FormatUsing(this string str, params string[] values)
+        {
+            return string.Format(str, values);
+        }
+    }
+}
