1f4c199dbf430b10b92a8ca38f1213a05f7a0b8b
Eddy Espinal <eespinal@intellisys.com.do>
2012-08-01 17:37:27 -0400

    Formatting LoginWindow

---
+++ src/TeamNotification_VisualStudio/TeamNotification_Library/Service/Async/IHandlePubSub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TeamNotification_Library.Service.Async
+{
+    public interface IHandlePubSub
+    {
+        void Publish<T>(T source, Func<T, CustomEventHandler> getter);
+    }
+
+    public class PubSubService : IHandlePubSub
+    {
+        public void Publish<T>(T source, Func<T, CustomEventHandler> getter)
+        {
+//            HandleEvent(getter(source));
+        }
+    }
+}
