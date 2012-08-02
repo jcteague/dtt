c6809957159480596a71f0f8befc4a05045c5e32
Eddy Espinal <eespinal@intellisys.com.do>
2012-08-02 17:48:38 -0400

    Removing old test

--- src/TeamNotification_VisualStudio/TeamNotification_Test/Library/Service/Http/ChatMessageSenderSpecs.cs
+++ src/TeamNotification_VisualStudio/TeamNotification_Test/Library/Service/Http/ChatMessageSenderSpecs.cs
@@ -14,23 +14,24 @@ namespace TeamNotification_Test.Library.Service.Http
             
         }
         
-        public class when_sending_a_message : Concern
-        {
-            Establish context = () =>
-            {
-                httpRequestsClient = depends.on<ISendHttpRequests>();
-                message = "blah";
-                url = "http://dtt.local:3000/room/1/messages";
-            };
-
-            Because b = () => sut.SendMessage(message, "1");
-
-            It should_send_the_message_in_the_url_using_the_client = () =>
-                httpRequestsClient.WasToldTo(x => x.Get(url));
-
-            private static ISendHttpRequests httpRequestsClient;
-            private static string message;
-            private static string url;
-        }
+        // TODO: This implementation may change
+//        public class when_sending_a_message : Concern
+//        {
+//            Establish context = () =>
+//            {
+//                httpRequestsClient = depends.on<ISendHttpRequests>();
+//                message = "blah";
+//                url = "http://dtt.local:3000/room/1/messages";
+//            };
+//
+//            Because b = () => sut.SendMessage(message, "1");
+//
+//            It should_send_the_message_in_the_url_using_the_client = () =>
+//                httpRequestsClient.WasToldTo(x => x.Get(url));
+//
+//            private static ISendHttpRequests httpRequestsClient;
+//            private static string message;
+//            private static string url;
+//        }
     }
 }
