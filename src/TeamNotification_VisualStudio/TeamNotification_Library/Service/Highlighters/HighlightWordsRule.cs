8a315eb676070bc2bb7075a38e8d7ad4d1b3ca1e
Eddy Espinal <eespinal@intellisys.com.do>
2012-08-20 15:12:12 -0400

    Separating RuleOptions and HighlightWords rules from Example

---
+++ src/TeamNotification_VisualStudio/TeamNotification_Library/Service/Highlighters/HighlightWordsRule.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+
+namespace TeamNotification_Library.Service.Highlighters
+{
+    public class HighlightWordsRule
+    {
+        public List<string> Words { get; private set; }
+        public RuleOptions Options { get; private set; }
+
+        public HighlightWordsRule()
+        {
+            Words = new List<string>();
+            Options = new RuleOptions("#00FFFF", "Bold", "Normal");
+
+            string[] words = ruleWords.ToArray();
+
+            foreach (string word in words)
+                if (!string.IsNullOrWhiteSpace(word))
+                    Words.Add(word.Trim());
+        }
+
+        private static List<string> ruleWords = new List<string> {
+                                                          "abstract",
+                                                          "as",
+                                                          "base",
+                                                          "bool",
+                                                          "break",
+                                                          "byte",
+                                                          "case",
+                                                          "catch",
+                                                          "char",
+                                                          "checked",
+                                                          "class",
+                                                          "const",
+                                                          "continue",
+                                                          "decimal",
+                                                          "default",
+                                                          "delegate",
+                                                          "do",
+                                                          "double",
+                                                          "else",
+                                                          "enum",
+                                                          "event",
+                                                          "explicit",
+                                                          "extern",
+                                                          "false",
+                                                          "finally",
+                                                          "fixed",
+                                                          "float",
+                                                          "for",
+                                                          "foreach",
+                                                          "goto",
+                                                          "if",
+                                                          "implicit",
+                                                          "in",
+                                                          "int",
+                                                          "interface",
+                                                          "internal",
+                                                          "is",
+                                                          "lock",
+                                                          "long",
+                                                          "namespace",
+                                                          "new",
+                                                          "null",
+                                                          "object",
+                                                          "operator",
+                                                          "out",
+                                                          "override",
+                                                          "params",
+                                                          "private",
+                                                          "protected",
+                                                          "public",
+                                                          "readonly",
+                                                          "ref",
+                                                          "return",
+                                                          "sbyte",
+                                                          "sealed",
+                                                          "short",
+                                                          "sizeof",
+                                                          "stackalloc",
+                                                          "static",
+                                                          "string",
+                                                          "struct",
+                                                          "switch",
+                                                          "this",
+                                                          "throw",
+                                                          "true",
+                                                          "try",
+                                                          "typeof",
+                                                          "uint",
+                                                          "ulong",
+                                                          "unchecked",
+                                                          "unsafe",
+                                                          "ushort",
+                                                          "using",
+                                                          "virtual",
+                                                          "void",
+                                                          "volatile",
+                                                          "while",
+                                                          "var"
+                                                      };
+    }
+}
---
+++ src/TeamNotification_VisualStudio/TeamNotification_Library/Service/Highlighters/RuleOptions.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace TeamNotification_Library.Service.Highlighters
+{
+    public class RuleOptions
+    {
+        public bool IgnoreCase { get; private set; }
+        public Brush Foreground { get; private set; }
+        public FontWeight FontWeight { get; private set; }
+        public FontStyle FontStyle { get; private set; }
+
+        public RuleOptions(string foreground, string fontWeight, string fontStyle)
+        {
+            string foregroundStr = foreground.Trim();
+            string fontWeightStr = fontWeight.Trim();
+            string fontStyleStr = fontStyle.Trim();
+
+            Foreground = (Brush)new BrushConverter().ConvertFrom(foregroundStr);
+            FontWeight = (FontWeight)new FontWeightConverter().ConvertFrom(fontWeightStr);
+            FontStyle = (FontStyle)new FontStyleConverter().ConvertFrom(fontStyleStr);
+        }
+    }
+}
