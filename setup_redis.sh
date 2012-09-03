191019bb68029e93d32cacf4fcc6b2f0a2011719
Eddy Espinal <eespinal@intellisys.com.do>
2012-09-03 16:21:44 -0400

    Making the redis-test a daemon and fixing copying of config

--- setup_redis.sh
+++ setup_redis.sh
@@ -3,5 +3,5 @@ tar xzf redis-2.4.15.tar.gz &&
 cd redis-2.4.15 &&
 make &&
 rm -f ../redis-2.4.15.tar.gz &&
-cp ../build_tools_templates/redis.conf . &&
-cp ../build_tools_templates/redis-test.conf .
+cp ../build_tools/templates/redis.conf . &&
+cp ../build_tools/templates/redis-test.conf .
