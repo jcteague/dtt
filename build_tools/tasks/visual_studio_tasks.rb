3855ad410fe639f88a956ead69b929dba09f992e
Eddy Espinal <eespinal@intellisys.com.do>
2012-10-04 16:35:55 -0400

    Updating visual studio tasks

--- build_tools/tasks/visual_studio_tasks.rb
+++ build_tools/tasks/visual_studio_tasks.rb
@@ -2,9 +2,13 @@ require File.join('./','build_tools','config.rb')
 require 'albacore'
 begin
     require 'FileUtils'
-    copy = FileUtils.copy
+    def copy(source, dest)
+    	FileUtils.copy source, dest
+    end
 rescue LoadError
-    copy = lambda {|source, dest| sh "cp #{source}, #{dest}"}
+    def copy(source, dest)
+    	sh "cp #{source}, #{dest}"
+    end
 end
 
 namespace :visual_studio do
