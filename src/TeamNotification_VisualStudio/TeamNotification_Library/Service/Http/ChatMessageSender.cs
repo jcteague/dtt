2861cf0b647453d7b1d7e43ef58f8cbe5c6f2d10
Xc <x@info.com>
2012-10-08 04:20:15 -0400

    Adding source for logging messages that didn't have it

--- src/TeamNotification_VisualStudio/TeamNotification_Library/Service/Http/ChatMessageSender.cs
+++ src/TeamNotification_VisualStudio/TeamNotification_Library/Service/Http/ChatMessageSender.cs
@@ -72,7 +72,7 @@ namespace TeamNotification_Library.Service.Http
             }
 
             AppendPlainMessage(messages, plainMessage, roomId);
-            logger.Info("Sending Message: {0}".FormatUsing(plainMessage));
+            logger.Info(this, "Sending Message: {0}".FormatUsing(plainMessage));
             client.Post(messages);
         }
 
