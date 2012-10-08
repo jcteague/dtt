c666132845e68e3cb8ab0f64e0796da060cba02c
Xc <x@info.com>
2012-10-08 03:32:17 -0400

    Logging using the source object in ChatRoomControlService

--- src/TeamNotification_VisualStudio/TeamNotification_Library/Service/Logging/Providers/NLogConfiguration.cs
+++ src/TeamNotification_VisualStudio/TeamNotification_Library/Service/Logging/Providers/NLogConfiguration.cs
@@ -8,31 +8,24 @@ namespace TeamNotification_Library.Service.Logging.Providers
 {
     public class NLogConfiguration : IConfigureLogging
     {
+        private string layout = "${date:format=yyyy-M-dd} ${time:format=HH:mm:ss} ${logger} : ${LEVEL}, ${message}";
+
         public void Initialize()
         {
             // Step 1. Create configuration object 
-            LoggingConfiguration config = new LoggingConfiguration();
+            var config = new LoggingConfiguration();
             
             // Step 2. Create targets and add them to the configuration 
-            ColoredConsoleTarget consoleTarget = new ColoredConsoleTarget();
-            config.AddTarget("console", consoleTarget);
-            
-            FileTarget fileTarget = new FileTarget();
+            var fileTarget = new FileTarget();
             config.AddTarget("file", fileTarget);
             
             // Step 3. Set target properties 
-            consoleTarget.Layout = "${date:format=HH\\:MM\\:ss} ${logger} ${message}";
             fileTarget.FileName = "${specialfolder:folder=LocalApplicationData}/" + GlobalConstants.Paths.LogFile;
-//            fileTarget.FileName = "${basedir}/dtt-yackety.txt";
-            fileTarget.Layout = "${date:format=yyyy-M-dd} ${time:format=HH:mm:ss} ${logger} : ${LEVEL}, ${message}";
-//            fileTarget.Layout = "${message}";
+            fileTarget.Layout = layout;
             
             // Step 4. Define rules
-            LoggingRule rule1 = new LoggingRule("*", LogLevel.Info, consoleTarget);
+            LoggingRule rule1 = new LoggingRule("*", LogLevel.Info, fileTarget);
             config.LoggingRules.Add(rule1);
-            
-            LoggingRule rule2 = new LoggingRule("*", LogLevel.Info, fileTarget);
-            config.LoggingRules.Add(rule2);
             
             // Step 5. Activate the configuration
             LogManager.Configuration = config;
