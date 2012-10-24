21eedc3ff25ba5ad4b74d0a6f8966bd6fa0d8940
Eddy Espinal <eespinal@intellisys.com.do>
2012-10-24 17:15:36 -0400

    Adding dialog to indicate the plugin was updated

--- src/TeamNotification_VisualStudio/TeamNotification_Library/Service/Update/DialogForUpdateFactory.cs
+++ src/TeamNotification_VisualStudio/TeamNotification_Library/Service/Update/DialogForUpdateFactory.cs
@@ -9,10 +9,12 @@ namespace TeamNotification_Library.Service.Update
     public class DialogForUpdateFactory : ICreateDialogForUpdate
     {
         private IRunInBackgroundWorker backgroundRunner;
+        private IHandleDialogMessages dialogMessagesEvents;
 
-        public DialogForUpdateFactory(IRunInBackgroundWorker backgroundRunner)
+        public DialogForUpdateFactory(IRunInBackgroundWorker backgroundRunner, IHandleDialogMessages dialogMessagesEvents)
         {
             this.backgroundRunner = backgroundRunner;
+            this.dialogMessagesEvents = dialogMessagesEvents;
         }
 
         public DialogMessageWasRequested GetInstance(IUpdatePackage packageUpdateManager, IInstalledExtension updatedExtension, IVsExtensionManager extensionManager, IVsExtensionRepository repoManager)
@@ -20,7 +22,14 @@ namespace TeamNotification_Library.Service.Update
             return new DialogMessageWasRequested
                                            {
                                                Message = "There is an update available for {0}. Would you like to download it?".FormatUsing(GlobalConstants.PackageName),
-                                               OkAction = () => backgroundRunner.Run(() => packageUpdateManager.UpdateIfAvailable(updatedExtension, extensionManager, repoManager)),
+                                               OkAction = () => backgroundRunner.Run(() =>
+                                                                                         {
+                                                                                             var updated = packageUpdateManager.UpdateIfAvailable(updatedExtension, extensionManager, repoManager);
+                                                                                             if (updated)
+                                                                                             {
+                                                                                                dialogMessagesEvents.OnAlertMessageRequested(this, new AlertMessageWasRequested {Message = "You must restart for the update to take effect."});
+                                                                                             }
+                                                                                         }),
                                                CancelAction = () => { }
                                            };
         }
