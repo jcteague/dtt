cc195d847d6fe84984b57f9bdbd44a6f5c2789c3
Enmanuel Toribio <eatskolnikov@Enmanuel-PC.(none)>
2012-11-27 14:22:13 -0400

    Published new version

---
+++ src/TeamNotification_VisualStudio/TeamNotification_Library/UI/Avalon/IAppendCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls.Primitives;
+using TeamNotification_Library.Models;
+
+namespace TeamNotification_Library.UI.Avalon
+{
+    public interface IAppendCode
+    {
+        void AppendCode(ChatMessageModel messages);
+    }
+}
