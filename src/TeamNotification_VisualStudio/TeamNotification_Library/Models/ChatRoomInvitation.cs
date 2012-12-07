13fd70a696cdc4e737eccff3a4fb2ff232a3856d
Enmanuel Toribio <eatskolnikov@Enmanuel-PC.(none)>
2012-12-07 10:48:25 -0400

    Added the ChatInvitation file

---
+++ src/TeamNotification_VisualStudio/TeamNotification_Library/Models/ChatRoomInvitation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamNotification_Library.Models
+{
+    public class ChatRoomInvitation
+    {
+        public string chat_room_name { get; set; }
+        public string chat_room_id { get; set; }
+        public User user { get; set; }
+        public string invited_user_id { get; set; }
+    }
+}
