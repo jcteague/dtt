046ce7c91783cbb9a34b74d77dd80d620aae2f35
Enmanuel Toribio <eats007@gmail.com>
2013-01-17 16:15:54 -0400

    The mute checkbox is now showing

--- src/TeamNotification_VisualStudio/TeamNotification_Package/Resources.Designer.cs
+++ src/TeamNotification_VisualStudio/TeamNotification_Package/Resources.Designer.cs
@@ -70,7 +70,7 @@ namespace AvenidaSoftware.TeamNotification_Package {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to YacketyApp - v1.0.12.
+        ///   Looks up a localized string similar to YacketyApp - v1.0.13.
         /// </summary>
         public static string ToolWindowTitle {
             get {
