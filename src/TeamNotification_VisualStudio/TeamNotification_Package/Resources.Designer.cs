ba310d7332f31165084eda99d1c9815f1c5943e2
Enmanuel Toribio <eats007@gmail.com>
2013-02-05 16:10:37 -0400

    Published new version

--- src/TeamNotification_VisualStudio/TeamNotification_Package/Resources.Designer.cs
+++ src/TeamNotification_VisualStudio/TeamNotification_Package/Resources.Designer.cs
@@ -70,7 +70,7 @@ namespace AvenidaSoftware.TeamNotification_Package {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to YacketyApp - v1.0.14.
+        ///   Looks up a localized string similar to YacketyApp - v1.0.16.
         /// </summary>
         public static string ToolWindowTitle {
             get {
